44d76be35d66b6bc600ab16165a331e7b33bf128
Josh Levinger <josh@levinger.net>
2015-01-27 17:02:13 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Call CBC for Net Neutrality
---
+++ _includes/share_tweet_text.js
@@ -0,0 +1,6 @@
+//
+//  WARNING! ~ If you need to use an apostrophe ==>'<==, precede it with a backslash
+//  So instead of "Bill O'Reilly" you would write "Bill O\'Reilly".
+//  If you fail to do this, everything will be ruined.
+//
+TWEET_TEXT = 'Who loves @Comcast? Tell these @GOP lawmakers to protect #netneutrality + stop shilling for Big Cable. http://TheseRepublicansLoveComcast.com';
